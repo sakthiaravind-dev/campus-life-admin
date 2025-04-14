090f84b1761c0a82bb6008c3b0462e0a792a43ac
sakthiaravind-dev <sakthicareer.edu@gmail.com>
2025-04-14 23:29:10 +0530

    changes

--- src/API/api.js
+++ src/API/api.js
@@ -1,12 +1,12 @@
 import axios from 'axios'
 
-export default axios.create({
-    baseURL:"https://campus-life-server.onrender.com"
-})
+//export default axios.create({
+  //  baseURL:"https://campus-life-server.onrender.com"
+//})
 
 
 // testing 
 
-// export default axios.create({
-//     baseURL:"http://localhost:3500"
-// })
+ export default axios.create({
+     baseURL:"http://localhost:3500"
+     })
